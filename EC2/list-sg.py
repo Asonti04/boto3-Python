ac847b43e5002c5e042531f131a7fd9095cd52db
Asonti <asontidg@gmail.com>
2023-06-16 03:20:03 +0000

    listing security groups

--- EC2/list-sg.py
+++ EC2/list-sg.py
@@ -5,4 +5,13 @@ ec2 = boto3.client('ec2')
 response = ec2.describe_security_groups()
 
 for sg in response['SecurityGroups']:
-    print(sg['GroupName'], sg['GroupId'], sg['VpcId'])
+    print(sg['GroupName'], sg['GroupId'], sg['VpcId'], sg['Description'])
+    
+   
+    for permissions in sg['IpPermissions']:
+        if 'FromPort' in permissions:
+            print(permissions['FromPort'], permissions['IpProtocol'], permissions['ToPort'])
+            
+        if 'IpRanges' in permissions:
+            for iprange in permissions['IpRanges']:
+                print(iprange['CidrIp'])
